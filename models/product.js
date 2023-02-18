3b10f21dabc32cd0151ed3cd78087b0109a5db08
Dina <Dina.essam.22.4.97@gmail.com>
2023-02-18 13:57:22 +0200

    edit product

--- models/product.js
+++ models/product.js
@@ -15,7 +15,8 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(title, imageUrl, description, price, id = null) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -23,10 +24,17 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
     getProductsFromFile((products) => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
+      const updatedProducts = [...products];
+      if (this.id) {
+        const existingProductIndex = products.findIndex((prod) => prod.id === this.id);
+        updatedProducts[existingProductIndex] = this;
+      }
+      else{
+        this.id = Math.random().toString();
+        updatedProducts.push(this);
+      }
+      fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
         console.log(err);
       });
     });
